e64d2122de5bf6c30ec7242ba7b8531de7c61b1a
Ben Overmyer <manatrance@gmail.com>
2016-05-03 13:45:15 -0500

    Initial commit.

---
+++ fetch-artifact.py
@@ -0,0 +1,39 @@
+import argparse, requests
+
+parser = argparse.ArgumentParser(description='The CircleCI artifact deployment tool.')
+parser.add_argument('-b', '--branch', help='Which branch to deploy (defaults to master)', default='master')
+parser.add_argument('-u', '--user', help='The CircleCI username to look under')
+parser.add_argument('-p', '--project', help='Which project to deploy')
+parser.add_argument('-t', '--token', help='The CircleCI API token to use')
+
+args = parser.parse_args()
+
+branch = args.branch
+user = args.user
+project = args.project
+token = args.token
+
+circleci = 'https://circleci.com/api/v1/project/' + user + '/' + project + '/latest/artifacts?circle-token=' + token + '&branch=' + branch
+
+print 'Checking for latest build...'
+
+response = requests.get( circleci )
+json = response.json()
+data = json[0]
+
+url = data['url']
+
+print 'Fetching artifact from ' + url + ' ...'
+
+url += '?circle-token=' + token
+
+response = requests.get( url, stream=True )
+
+filename = 'latest.tar.gz'
+chunk_size = 1024
+
+with open( filename, 'wb' ) as fd:
+  for chunk in response.iter_content( chunk_size ):
+    fd.write( chunk )
+
+print 'Artifact retrieved and saved as ' + filename + '.'
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Fetch Artifact
+
+Fetches a build artifact from CircleCI.
+
+## Arguments:
+
+* `-b`, `--branch` The project branch (defaults to master)
+* `-p`, `--project` The project to pull from
+* `-u`, `--user` The CircleCI username to check under
+* `-t`, `--token` Your CircleCI API token
